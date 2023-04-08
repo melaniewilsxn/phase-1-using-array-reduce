168325122cbfabdd2257c68d0124a2f03620b0a5
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-08 14:53:45 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const totalBatteries = batteryBatches.reduce(batteryCounter, 0)
+// const totalBatteries = batteryBatches.reduce(batteryCounter, 0)
 
-function batteryCounter(total, num){
-    return total + num;
-}
+// function batteryCounter(total, num){
+//     console.log(total, num)
+//     return total + num;
+// }
+
+// const totalBatteries = batteryBatches.reduce(function(total, currentBatteryNum){
+//     return total + currentBatteryNum
+// })
+
+const totalBatteries = batteryBatches.reduce((total, currentBatteryNum) =>  total + currentBatteryNum)
